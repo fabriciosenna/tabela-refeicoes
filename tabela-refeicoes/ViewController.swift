3b1cf9a3e80e45dbec007c60e83fca237cad9206
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-18 16:42:46 -0300

    novo projeto usando table view no storyboard

---
+++ tabela-refeicoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabela-refeicoes
+//
+//  Created by Fabricio Rodrigo Baixo on 12/18/20.
+//  Copyright © 2020 Fabricio Rodrigo Baixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
